a952807e146168b491e9b45e75a342ab93b1387f
zhangsan <971083603@qq.com>
2017-07-29 17:30:08 +0800

    ok====================

--- 魔方/nfc/src/com/jyw/controller/business/Daily_menuController.java
+++ 魔方/nfc/src/com/jyw/controller/business/Daily_menuController.java
@@ -14,26 +14,26 @@ import org.springframework.web.servlet.ModelAndView;
 import com.jyw.controller.base.BaseController;
 import com.jyw.entity.Page;
 import com.jyw.entity.system.Menu;
-import com.jyw.service.business.LunchService;
+import com.jyw.service.business.Daily_menuService;
 import com.jyw.util.Const;
 import com.jyw.util.PageData;
 import com.jyw.util.ServiceHelper;
 
 /** 
- * 类名称：lunchController
+ * 类名称：daily_menuController
  * 创建人：魏汉文
  * 创建时间：2017-07-28
  */
 @Controller
-@RequestMapping(value="/lunch")
-public class LunchController extends BaseController {
+@RequestMapping(value="/daily_menu")
+public class Daily_menuController extends BaseController {
 	
-	@Resource(name="lunchService")
-	private LunchService lunchService;
+	@Resource(name="daily_menuService")
+	private Daily_menuService daily_menuService;
 	
 	/**
 	 * 新增
-	 * lunch/save.do
+	 * daily_menu/save.do
 	 */
 	@RequestMapping(value="/save")
 	public ModelAndView save() throws Exception{
@@ -41,7 +41,7 @@ public class LunchController extends BaseController {
 		PageData pd = new PageData();
 		try {
 			pd = this.getPageData();
-			lunchService.save(pd);
+			daily_menuService.save(pd);
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -53,14 +53,14 @@ public class LunchController extends BaseController {
 	
 	/**
 	 * 删除
-	 * lunch/delete.do
+	 * daily_menu/delete.do
 	 */
 	@RequestMapping(value="/delete")
 	public void delete(PrintWriter out){
  		PageData pd = new PageData();
 		try{
 			pd = this.getPageData();
-			lunchService.delete(pd);
+			daily_menuService.delete(pd);
 			out.write("success");
 			out.close();
 		} catch(Exception e){
@@ -69,28 +69,12 @@ public class LunchController extends BaseController {
 		
 	}
 	
-	/**
-	 * 修改排序
-	 * lunch/editSort.do
-	 */
-	@RequestMapping(value="/editSort")
-	public void editSort(PrintWriter out){
-		PageData pd = new PageData();
-		try{
-			pd = this.getPageData();
-			lunchService.editSort(pd);
-			out.write("success");
-			out.close();
-		} catch(Exception e){
-			logger.error(e.toString(), e);
-		}
-		
-	}
+ 
 	 
 	
 	/**
 	 * 修改
-	 * lunch/edit.do
+	 * daily_menu/edit.do
 	 */
 	@RequestMapping(value="/edit")
 	public ModelAndView edit() throws Exception{
@@ -98,7 +82,7 @@ public class LunchController extends BaseController {
 		PageData pd = new PageData();
 		try {
 			pd = this.getPageData();
-			lunchService.edit(pd);
+			daily_menuService.edit(pd);
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -110,7 +94,7 @@ public class LunchController extends BaseController {
 	
 	/**
 	 * 列表
-	 * lunch/list.do
+	 * daily_menu/list.do
 	 */
 	@RequestMapping(value="/list")
 	public ModelAndView list(Page page){
@@ -119,9 +103,20 @@ public class LunchController extends BaseController {
 		try{
 			pd = this.getPageData();
 			page.setPd(pd);
-			List<PageData>	varList = lunchService.list(page);	//列出W列表
+			List<PageData>	varList = daily_menuService.list(page);	//列出W列表
+			for (PageData e : varList) {
+				String lunch_namestr="";
+				String lunch_idstr=e.getString("lunch_idstr");
+				String[] str=lunch_idstr.split(",");
+				for (int i = 0; i < str.length; i++) {
+					pd.put("lunch_id", str[i]);
+					lunch_namestr+=ServiceHelper.getLunchService().findById(pd).getString("lunch_namestr");
+ 				}
+				e.put("lunch_namestr", lunch_namestr);
+			}
+			pd.remove("lunch_id");
 			this.getHC(); //调用权限
-			mv.setViewName("business/lunch/lunch_list");
+			mv.setViewName("business/daily_menu/daily_menu_list");
 			mv.addObject("varList", varList);
 			mv.addObject("pd", pd);
 		} catch(Exception e){
@@ -132,7 +127,7 @@ public class LunchController extends BaseController {
 	
 	/**
 	 * 去新增页面
-	 * lunch/goAdd.do
+	 * daily_menu/goAdd.do
 	 */
 	@RequestMapping(value="/goAdd")
 	public ModelAndView goAdd(){
@@ -140,10 +135,10 @@ public class LunchController extends BaseController {
 		PageData pd = new PageData();
  		try {
  			pd = this.getPageData();
- 			//获取属性列表
-			List<PageData> cateList=ServiceHelper.getCategoryService().listAll(pd);
-			mv.addObject("cateList", cateList);
-			mv.setViewName("business/lunch/lunch_save");
+ 			//获取便当列表
+			List<PageData> lunchList=ServiceHelper.getLunchService().listAllUpShelves(pd);
+			mv.addObject("lunchList", lunchList);
+			mv.setViewName("business/daily_menu/daily_menu_save");
  			mv.addObject("pd", pd);
 		} catch (Exception e) {
 			logger.error(e.toString(), e);
@@ -153,7 +148,7 @@ public class LunchController extends BaseController {
 	
 	/**
 	 * 去修改页面
-	 * lunch/goEdit.do
+	 * daily_menu/goEdit.do
 	 */
 	@RequestMapping(value="/goEdit")
 	public ModelAndView goEdit(){
@@ -161,12 +156,12 @@ public class LunchController extends BaseController {
 		PageData pd = new PageData();
  		try {
  			pd = this.getPageData();
- 			//获取属性列表
-			List<PageData> cateList=ServiceHelper.getCategoryService().listAll(pd);
-			mv.addObject("cateList", cateList);
-			pd = lunchService.findById(pd);	//根据ID读取
+ 			//获取便当列表
+			List<PageData> lunchList=ServiceHelper.getLunchService().listAllUpShelves(pd);
+			mv.addObject("lunchList", lunchList);
+			pd = daily_menuService.findById(pd);	//根据ID读取
 			mv.addObject("pd", pd);
-			mv.setViewName("business/lunch/lunch_edit");
+			mv.setViewName("business/daily_menu/daily_menu_edit");
 			mv.addObject("msg", "edit");
 			
 		} catch (Exception e) {
